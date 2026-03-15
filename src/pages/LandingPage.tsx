8535f4ecb2dadaacb8b0a9b474e97f32305eca03
michal <stachnikmichal@gmail.com>
2026-03-15 11:27:16 -0400

    hiding 3d scene for mobile

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -128,7 +128,7 @@ export default function LandingPage() {
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.8, delay: 0.2 }}
-            className="h-[420px] lg:h-[500px]"
+            className="hidden lg:block h-[420px] lg:h-[500px]"
           >
             <SceneWrapper
               height={500}
